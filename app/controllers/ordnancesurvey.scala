ea9150d9ac841274055c732fb4639e316c5832d0
andy1138 <andy@hicks.net>
2016-02-20 12:02:56 +0000

    Add url to do manual download

---
+++ app/controllers/ordnancesurvey.scala
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import java.io._
+
+import com.typesafe.config.ConfigFactory
+import play.api.libs.iteratee.Enumerator
+import services.addressimporter.converter.{Extractor, CSVLine}
+import services.addressimporter.ftpdownloader.{RealWorldIO, FtpDownloader}
+import uk.gov.hmrc.play.microservice.controller.BaseController
+import uk.gov.hmrc.play.http.logging.MdcLoggingExecutionContext._
+import play.api.mvc._
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success, Try}
+
+
+object OrdnanceSurveyController extends OrdnanceSurveyController
+
+trait OrdnanceSurveyController extends Controller {
+
+  import services.addressimporter.converter.Extractor
+  import scala.concurrent.ExecutionContext.Implicits.global
+
+
+  def manualImport(): Action[AnyContent] = Action {
+
+    val out: PipedOutputStream = new PipedOutputStream()
+
+    val data: InputStream = new BufferedInputStream(new PipedInputStream(out))
+
+    val dataContent: Enumerator[Array[Byte]] = Enumerator.fromStream(data).andThen(Enumerator.eof)
+
+
+    Future {
+
+      Try {
+        val conf = ConfigFactory.load()
+
+        // os server details
+        val server = conf.getString("app.os.server")
+        val port = conf.getString("app.os.port")
+        val user = conf.getString("app.os.user")
+        val pass = conf.getString("app.os.pass")
+        val osHomeFolder = conf.getString("app.os.homeFolder")
+
+        // temp folder details
+        val tmpZipfilesHome = conf.getString("app.temp.folder")
+
+
+        val ftpClient = new FtpDownloader with RealWorldIO
+        ftpClient.FileCreationDelay = 1
+
+        ftpClient.login(user, pass, server).map { result =>
+          ftpClient.ftpDownload(osHomeFolder, tmpZipfilesHome) match {
+            case Success((count, subFolder)) =>
+
+              val csvOut = (line: CSVLine) => {
+                // scalastyle:off
+                out.write(line.toString.getBytes)
+                out.write("\n".getBytes)
+              }
+              val result = Extractor.extract(new File(tmpZipfilesHome + "/" + subFolder), csvOut)
+
+            case Failure(err) =>
+              err.printStackTrace()
+          }
+        }
+        data.close()
+        out.flush()
+        out.close()
+        ftpClient.logout()
+
+      }.recoverWith { case x =>
+        x.printStackTrace()
+        throw x
+      }
+    }
+
+    Ok.chunked(dataContent).as("text/csv")
+  }
+}
