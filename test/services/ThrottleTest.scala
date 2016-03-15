93b85b0638a4e639f7d83eac52c20e2fcdcfb302
Rick <rickb777@github.com>
2016-03-15 12:20:07 +0000

    Added licence block. Reverted the inlining of test descriptions.

--- test/services/ThrottleTest.scala
+++ test/services/ThrottleTest.scala
@@ -1,13 +1,33 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package services
 
-import org.mockito.Matchers._
 import org.mockito.Mockito._
 import org.scalatest.FunSuite
 import org.scalatest.mock.MockitoSugar
 
 class ThrottleTest extends FunSuite with MockitoSugar {
 
-  test( """ When a throttle is used with a periodic setting then the sleep time would have been period * numItems.""") {
+  test(
+    """
+       Given that time ticks are stubbed to be advancing very slowly,
+       When a throttle is used with a periodic setting,
+       Then the sleep time would have been period * numItems.
+    """) {
     val time = mock[Time]
     val throttle = new Throttle(10, 0, time)
 
@@ -20,7 +40,12 @@ class ThrottleTest extends FunSuite with MockitoSugar {
     verify(time).sleep(70)
   }
 
-  test( """ When a throttle is used with a backoff setting then the sleep time would have been period * numItems.""") {
+  test(
+    """
+       Given that time ticks are stubbed to be advancing very slowly,
+       When a throttle is used with a backoff setting,
+       Then the sleep time would have been period * numItems.
+    """) {
     val time = mock[Time]
     val throttle = new Throttle(0, 10, time)
 
