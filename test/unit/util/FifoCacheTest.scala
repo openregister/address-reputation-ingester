ee31a352356e5d678d4dc418215ff6d53b450928
Rick <rickb777@github.com>
2016-10-05 16:30:32 +0100

    Compilation issue fixed

--- test/unit/util/FifoCacheTest.scala
+++ test/unit/util/FifoCacheTest.scala
@@ -20,24 +20,24 @@
 package util
 
 import org.scalatest.FunSuite
-import uk.co.hmrc.address.osgb.DbAddress
+import uk.gov.hmrc.address.osgb.DbAddress
 
 class FifoCacheTest extends FunSuite {
 
-  val a01 = DbAddress("GB100040230002", List("6 Prospect Gardens"), Some("Exeter"), "EX4 6TA", Some("GB-ENG"), Some("UK"), Some(1110), Some("en"), Some(3), Some(1), None)
-  val a02 = DbAddress("GB10023118140", List("3 Terracina Court", "Haven Road"), Some("Exeter"), "EX2 8DP", Some("GB-ENG"), Some("UK"), Some(1110), Some("en"), Some(2), Some(1), None)
-  val a03 = DbAddress("GB10013050866", List("9 Princesshay"), Some("Exeter"), "EX1 1GE", Some("GB-ENG"), Some("UK"), Some(1110), Some("en"), None, Some(8), Some(8))
-  val a04 = DbAddress("GB10091471879", List("Flat 1", "2 Queens Crescent"), Some("Exeter"), "EX4 6AY", Some("GB-ENG"), Some("UK"), Some(1110), Some("en"), Some(2), Some(1), Some(8))
-  val a05 = DbAddress("GB10091472481", List("Unit 1", "25 Manor Road"), Some("Exeter"), "EX4 1BU", Some("GB-ENG"), Some("UK"), Some(1110), Some("en"), Some(2), Some(1), Some(8))
-  val a06 = DbAddress("GB10091471884", List("Room 4, Baring House", "6 Baring Crescent"), Some("Exeter"), "EX1 1TL", Some("GB-ENG"), Some("UK"), Some(1110), Some("en"), Some(2), Some(1), Some(8))
-  val a07 = DbAddress("GB10091471886", List("Office 7, Baring House", "6 Baring Crescent"), Some("Exeter"), "EX1 1TL", Some("GB-ENG"), Some("UK"), Some(1110), Some("en"), Some(2), Some(1), Some(8))
-  val a08 = DbAddress("GB10023117655", List("School Kitchen, St Michaels Ce Primary School", "South Lawn Terrace"), Some("Exeter"), "EX1 2SN", Some("GB-ENG"), Some("UK"), Some(1110), Some("en"), Some(2), Some(1), Some(8))
-  val a09 = DbAddress("GB10023119039", List("Annexe", "12 St Leonards Road"), Some("Exeter"), "EX2 4LA", Some("GB-ENG"), Some("UK"), Some(1110), Some("en"), Some(2), Some(1), Some(8))
-  val a10 = DbAddress("GB100040210161", List("1 Curlew Way"), Some("Exeter"), "EX4 4SW", Some("GB-ENG"), Some("UK"), Some(1110), Some("en"), Some(2), Some(1), None)
-  val a11 = DbAddress("GB10023122465", List("15 Gate Reach"), Some("Exeter"), "EX2 6GA", Some("GB-ENG"), Some("UK"), Some(1110), Some("en"), Some(2), Some(1), None)
-  val a12 = DbAddress("GB10023119082", List("Flat G.01 Block G, Birks Hall", "New North Road"), Some("Exeter"), "EX4 4FT", Some("GB-ENG"), Some("UK"), Some(1110), Some("en"), Some(2), Some(1), None)
-  val a13 = DbAddress("GB10092760043", List(), Some("Exeter"), "EX1 9UL", Some("GB-ENG"), Some("UK"), Some(1110), Some("en"), None, Some(1), None)
-  val a14 = DbAddress("GB10013038314", List("Unit 25, Exeter Business Centre", "39 Marsh Green Road West", "Marsh Barton Trading Estate"), Some("Exeter"), "EX2 8PN", Some("GB-ENG"), Some("UK"), Some(1110), Some("en"), Some(2), Some(1), None)
+  val a01 = DbAddress("GB100040230002", List("6 Prospect Gardens"), Some("Exeter"), "EX4 6TA", Some("GB-ENG"), Some("UK"), Some(1110), Some("en"), Some(3), Some(1), None, None, None)
+  val a02 = DbAddress("GB10023118140", List("3 Terracina Court", "Haven Road"), Some("Exeter"), "EX2 8DP", Some("GB-ENG"), Some("UK"), Some(1110), Some("en"), Some(2), Some(1), None, None, None)
+  val a03 = DbAddress("GB10013050866", List("9 Princesshay"), Some("Exeter"), "EX1 1GE", Some("GB-ENG"), Some("UK"), Some(1110), Some("en"), None, Some(8), Some(8), None, None)
+  val a04 = DbAddress("GB10091471879", List("Flat 1", "2 Queens Crescent"), Some("Exeter"), "EX4 6AY", Some("GB-ENG"), Some("UK"), Some(1110), Some("en"), Some(2), Some(1), Some(8), None, None)
+  val a05 = DbAddress("GB10091472481", List("Unit 1", "25 Manor Road"), Some("Exeter"), "EX4 1BU", Some("GB-ENG"), Some("UK"), Some(1110), Some("en"), Some(2), Some(1), Some(8), None, None)
+  val a06 = DbAddress("GB10091471884", List("Room 4, Baring House", "6 Baring Crescent"), Some("Exeter"), "EX1 1TL", Some("GB-ENG"), Some("UK"), Some(1110), Some("en"), Some(2), Some(1), Some(8), None, None)
+  val a07 = DbAddress("GB10091471886", List("Office 7, Baring House", "6 Baring Crescent"), Some("Exeter"), "EX1 1TL", Some("GB-ENG"), Some("UK"), Some(1110), Some("en"), Some(2), Some(1), Some(8), None, None)
+  val a08 = DbAddress("GB10023117655", List("School Kitchen, St Michaels Ce Primary School", "South Lawn Terrace"), Some("Exeter"), "EX1 2SN", Some("GB-ENG"), Some("UK"), Some(1110), Some("en"), Some(2), Some(1), Some(8), None, None)
+  val a09 = DbAddress("GB10023119039", List("Annexe", "12 St Leonards Road"), Some("Exeter"), "EX2 4LA", Some("GB-ENG"), Some("UK"), Some(1110), Some("en"), Some(2), Some(1), Some(8), None, None)
+  val a10 = DbAddress("GB100040210161", List("1 Curlew Way"), Some("Exeter"), "EX4 4SW", Some("GB-ENG"), Some("UK"), Some(1110), Some("en"), Some(2), Some(1), None, None, None)
+  val a11 = DbAddress("GB10023122465", List("15 Gate Reach"), Some("Exeter"), "EX2 6GA", Some("GB-ENG"), Some("UK"), Some(1110), Some("en"), Some(2), Some(1), None, None, None)
+  val a12 = DbAddress("GB10023119082", List("Flat G.01 Block G, Birks Hall", "New North Road"), Some("Exeter"), "EX4 4FT", Some("GB-ENG"), Some("UK"), Some(1110), Some("en"), Some(2), Some(1), None, None, None)
+  val a13 = DbAddress("GB10092760043", List(), Some("Exeter"), "EX1 9UL", Some("GB-ENG"), Some("UK"), Some(1110), Some("en"), None, Some(1), None, None, None)
+  val a14 = DbAddress("GB10013038314", List("Unit 25, Exeter Business Centre", "39 Marsh Green Road West", "Marsh Barton Trading Estate"), Some("Exeter"), "EX2 8PN", Some("GB-ENG"), Some("UK"), Some(1110), Some("en"), Some(2), Some(1), None, None, None)
 
   test("size, get and maximum limit") {
     val cache = new FifoCache[String, DbAddress](5, a => a.id)
