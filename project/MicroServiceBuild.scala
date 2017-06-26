ef62184aebeeb6f62755dce0a0718a6a97477135
Richard Shepherd <richard.shepherd@digital.hmrc.gov.uk>
2017-06-26 14:37:31 +0100

    TXMNT-771 Update store version.

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -44,7 +44,7 @@ private object AppDependencies {
     "uk.gov.hmrc" %% "play-url-binders" % "2.0.0",
     "uk.gov.hmrc" %% "play-config" % "3.0.0",
     "uk.gov.hmrc" %% "logback-json-logger" % "3.1.0",
-    "uk.gov.hmrc" %% "address-reputation-store" % "2.22.0" withSources(),
+    "uk.gov.hmrc" %% "address-reputation-store" % "2.23.0" withSources(),
     "com.sksamuel.elastic4s" %% "elastic4s-core" % "2.4.0",
     "com.github.lookfirst" % "sardine" % "5.7",
     "net.openhft" % "chronicle-map" % "3.8.0",
